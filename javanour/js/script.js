f629c6af4bc40512569e34844b22fce25ec005c7
Abanoub Yacoub <96332371+abanoubyacoub@users.noreply.github.com>
2022-02-20 15:41:35 +0400

    2002

--- javanour/js/script.js
+++ javanour/js/script.js
@@ -1,1 +1,23 @@
-var hi = "Hello world";
+var menuItem =document.getElementsByTagName("li");
+var status =document.getElementById("title");
+// menuItem[0].innerHTML = "Hello world ";
+// menuItem[1].innerHTML = "عزيز وغالي ";
+// menuItem[2].innerHTML = "عزيز وغالي ";
+// menuItem[3].innerHTML = "عزيز وغالي ";
+// menuItem[4].innerHTML = "عزيز وغالي ";
+
+/*to replace typing 4 line to one line */
+
+// for (var x = 0; x < menuItem.length; x++) {
+// 	menuItem[x].innerHTML = "Hello world";
+// }
+
+for (var x = 0; x < menuItem.length; x++) {
+	menuItem[x].addEventListener("click", showItem);
+}
+
+function showItem(){
+	alert("اهلا بيك ")
+	status="hello World"
+}
+
